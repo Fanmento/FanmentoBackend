76d72a8f0ac86512cc81a3581bc25979956d33af
Dean Andreakis <deanandreakis@gmail.com>
2015-08-18 19:30:39 -0500

    Production Release on GAE

--- gae_utilities/models.py
+++ gae_utilities/models.py
@@ -1,5 +1,10 @@
+from __future__ import with_statement
+
 from google.appengine.ext import db, blobstore
-from google.appengine.api import files
+import cloudstorage as gcs
+import os
+import uuid
+from google.appengine.api import app_identity
 from google.appengine.api.images import get_serving_url
 
 from django.http import HttpResponse
@@ -17,18 +22,20 @@ class BlobModel(db.Model):
         if filename.split('.')[-1] == 'jpg' or filename.split('.')[-1] == 'jpeg':
             mime_type = 'image/jpeg'
 
-        # Create the file
-        file_name = files.blobstore.create(mime_type=mime_type)
+        bucket_name = os.environ.get('BUCKET_NAME', app_identity.get_default_gcs_bucket_name())
+        bucket = '/' + bucket_name
+        filename_final = bucket + '/' + str(uuid.uuid4())
 
-        # Open the file and write to it
-        with files.open(file_name, 'a') as f:
+        # Create a GCS file with GCS client.
+        with gcs.open(filename_final, 'w') as f:
             f.write(blob)
-
-        # Finalize the file. Do this before attempting to read it.
-        files.finalize(file_name)
+            f.close()
+       
+        # Blobstore API requires extra /gs to distinguish against blobstore files.
+        blobstore_filename = '/gs' + filename_final
 
         # Get the file's blob key
-        blob_key = files.blobstore.get_blob_key(file_name)
+        blob_key = blobstore.create_gs_key(blobstore_filename)
         # Store it
         self.image = blob_key
         self.mime_type = mime_type
